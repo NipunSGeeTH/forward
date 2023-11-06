800dd090808617f0fcbaf2362f91ddb6708a639a
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-06 23:29:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
 import telegram
 from telegram.ext import Updater, CommandHandler, Filters
 
-# Replace '2118571380:AAGR-_rB53MsMon35q5i2B3Nw7RJqPXHy18' with your bot token
-updater = Updater('YOUR_BOT_TOKEN')
+updater = Updater('2118571380:AAGR-_rB53MsMon35q5i2B3Nw7RJqPXHy18')
 dispatcher = updater.dispatcher
 
 def start(update, context):
-    update.message.reply_text('Hi!')
+    update.message.reply_text('Hi there! How can I help you today?')
 
 dispatcher.add_handler(CommandHandler('start', start))
 
