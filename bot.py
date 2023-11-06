1309032b95302026159e794a1513cac43e26cd72
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-06 23:55:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ BOT_TOKEN = '2118571380:AAGR-_rB53MsMon35q5i2B3Nw7RJqPXHy18'
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-@bot.message_handler(commands=['start'])
-def send_welcome_message(message):
-    bot.send_message(message.chat.id, 'Hi!')
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.send_message(message.chat.id, message.text)
 
 bot.polling()
